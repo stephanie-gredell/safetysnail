1bccc6e58f30b2089e082b578e610366369ec464
Stephanie Chung <s.raide@gmail.com>
2019-07-09 23:23:55 -0700

    Create React component for registration and ratings

---
+++ src/Ratings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Ratings extends React.Component {
+  render() {
+    return(
+<div>
+      <button>Thumbs up</button>
+      <button>Thumbs down</button>
+</div>
+    );
+  }
+}
--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -5,7 +5,9 @@ export default class RegistrationForm extends React.Component {
     return(
       <form>
         <input type="text" name="username" placeholder="Username"/>
-        <input type="password" name="password" placeholder="Password" />
+        <input type="text" name="email"  placeholder="Email address"/>
+        <input type="password" name="password" placeholder="Enter a password" />
+        <input type="password" name="password-confirmation" placeholder="Enter password again" />
         <input type="submit" value="Log in" />
       </form>
     );
