ba93870b46159014ed163d4cef04ea1547ad5d07
Stephanie Chung <s.raide@gmail.com>
2019-07-10 23:50:44 -0700

    Add cookie setting logic and redirects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.scss';
+import RegistrationForm from "./RegistrationForm";
 import SearchForm from "./SearchForm";
-import Navigation from "./Navigation"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LoginForm from "./LoginForm"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Redirect } from 'react-router'
+import Cookies from 'universal-cookie';
 
 export default function App() {
+  const cookies = new Cookies();
+  const loggedInCookie = cookies.get('login');
+  const isLoggedIn = typeof loggedInCookie !== 'undefined';
+
   return (
     <Router>
-      <Navigation />
-      <Route exact path="/" component={SearchForm} />
+      {/* check if user is logged in */}
+      <Route exact path="/" render={() => (
+        isLoggedIn ? (
+          <Redirect to="/search/youtube"/>
+        ) : (
+          <Redirect to="/login"/>
+        )
+      )}/>
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/register" component={RegistrationForm} />
+      <Route exact path="/search/youtube" component={SearchForm} />
     </Router>
 )}
